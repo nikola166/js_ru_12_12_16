57abdebaf8aec773531d2f1e48e670e6c86d1fe0
Roman <kaboom.ua@gmail.com>
2017-01-16 10:58:05 +0200

    Update gen.js

--- src/middlewares/gen.js
+++ src/middlewares/gen.js
@@ -7,9 +7,11 @@ function generateId() {
 export default store => next => action => {
     const {type, payload} = action;
     switch (type) {
+    //через мидлвары будет проходить каждый экшин, они должны быть максимально общими, завязывать на конкретные экшины - плохая практика
         case ADD_COMMENT:
             let newPayload = Object.assign({}, payload);
             newPayload.comment.id = generateId();
+            //Object.assign здесь лишний
             return next(Object.assign({}, {type: type, payload:newPayload}));
             break
         default:
